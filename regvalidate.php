68ff5d4550acc7dbafd250e25a9971524125a594
Adejobi Ezekiel <31517324+Ezeko@users.noreply.github.com>
2019-09-18 20:42:13 +0100

    Update regvalidate.php

--- regvalidate.php
+++ regvalidate.php
@@ -9,32 +9,55 @@ $password2 =$_POST['confirmPassword'];
             //check if user exist
             $file = fopen("data.txt", "r");
             $finduser = false;
-
+            $findemail = false;
+            
             while(!feof($file)){
                 $line = fgets($file);
                 $array = explode(";", $line);
+                
+                //initializing array
+                if(!isset($array[1])){
+                    $array[1]="";
+                }
+                if(!isset($array[2])){
+                    $array[2]="";
+                }
+                
+                
                 if(trim($array[1]) == $username){
                     $finduser = true;
+                    break;
+                }
+                if(trim($array[2]) == $email){
+                    $findemail = true;
                     break;
                 }
             }
             fclose($file);
 
             if ($finduser){
-                echo ("$username already registered");
-                header("Refresh:4, url=register.html");
+                    header("Refresh:4, url=register.html");
+                echo (" User: $username already registered");
+            }else if($findemail){
+                    header("Refresh:4, url=register.html");
+                echo (" Email: $email already registered");
             }else{
                 $file = fopen("data.txt", "a");
                 fputs($file, $fullname.";".$username.";".$email.";". $password."\r\n");
-                die("$username REGISTRATION SUCCESSFUL!!!"); 
                 header("Refresh:4, url=login.html");
+                $user = strtoupper($username);
+                die(" User: $user REGISTRATION SUCCESSFUL!!!"); 
+        
                     }
 
         }else {
-            echo "<script>alert('Password doesn't match'); window.location.replace('register.html');</script>";
+            
+            header("Refresh:4, url=register.html");
+            die("Password doesn't match");
         }
     }else{
-        echo "<script>alert('Some field(s) is(are) empty!!! Check that you fill the correct details in all the fields'); window.location.replace('register.html');</script>";
+        header("Refresh:3, url=register.html");
+        die("Some field(s) is(are) empty!!! Check that you fill the correct details in all the fields");
     }
     
 
